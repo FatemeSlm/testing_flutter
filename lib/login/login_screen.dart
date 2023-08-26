f16804f002f341ab58774eff3b4b50ba99808301
salameh.fatemeh <salameh.fatemeh@okco.ir>
2023-08-26 14:38:06 +0330

    add widget test

--- lib/login/login_screen.dart
+++ lib/login/login_screen.dart
@@ -1,10 +1,58 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:unit_widget_integration_test_sample/validator.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final emailController = TextEditingController(text: '');
+  final passwordController = TextEditingController(text: '');
+  final formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login Screen'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: emailController,
+                key: const ValueKey('emailId'),
+                decoration: const InputDecoration(hintText: 'Enter Email id'),
+                validator: (value) => Validator.validateEmail(value ?? ''),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                controller: passwordController,
+                key: const ValueKey('password'),
+                decoration: const InputDecoration(hintText: 'Enter Password'),
+                validator: (value) => Validator.validatePassword(value ?? ''),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: () {
+                if(formKey.currentState!.validate()){
+
+                }
+              }, child: const Text('Login')),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ test/login_screen_test.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unit_widget_integration_test_sample/login/login_screen.dart';
+
+void main() {
+  testWidgets('should have a title', (WidgetTester tester) async {
+    //arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+
+    // act
+    Finder titleText = find.text('Login Screen');
+
+    // assert
+    expect(titleText, findsOneWidget);
+  });
+
+  testWidgets('should have a text form filed to collect user email',
+      (WidgetTester tester) async {
+    //arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+
+    // act
+    Finder userNameTextFiled = find.byKey(const ValueKey('emailId'));
+
+    // assert
+    expect(userNameTextFiled, findsOneWidget);
+  });
+
+  testWidgets('should have a text form filed to collect user password',
+      (WidgetTester tester) async {
+    //arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+
+    // act
+    Finder passwordTextFiled = find.byKey(const ValueKey('password'));
+
+    // assert
+    expect(passwordTextFiled, findsOneWidget);
+  });
+
+  testWidgets('should have a login button', (WidgetTester tester) async {
+    //arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+
+    // act
+    Finder loginButton = find.byType(ElevatedButton);
+
+    // assert
+    expect(loginButton, findsOneWidget);
+  });
+
+  testWidgets(
+      'should show Required Field error messages if user email and password is empty',
+      (WidgetTester tester) async {
+    //arrange
+    await tester.pumpWidget(const MaterialApp(
+      home: LoginScreen(),
+    ));
+
+    // act
+    Finder loginButton = find.byType(ElevatedButton);
+    await tester.tap(loginButton);
+    await tester.pumpAndSettle();
+
+    Finder errorTexts = find.text('Required Filed');
+
+    // assert
+    expect(errorTexts, findsNWidgets(2));
+  });
+
+  testWidgets(
+      'should show invalid email error message if user email is invalid',
+          (WidgetTester tester) async {
+        //arrange
+        await tester.pumpWidget(const MaterialApp(
+          home: LoginScreen(),
+        ));
+
+        // act
+        Finder emailTextField = find.byKey(const ValueKey('emailId'));
+        await tester.enterText(emailTextField, 'hjdfghg');
+
+        Finder loginButton = find.byType(ElevatedButton);
+        await tester.tap(loginButton);
+        await tester.pumpAndSettle();
+
+        Finder emailInvalidText = find.text('Please enter a valid email');
+
+        // assert
+        expect(emailInvalidText, findsOneWidget);
+      });
+
+  testWidgets(
+      'should show invalid password error message if user password is invalid',
+          (WidgetTester tester) async {
+        //arrange
+        await tester.pumpWidget(const MaterialApp(
+          home: LoginScreen(),
+        ));
+
+        // act
+        Finder passwordTextField = find.byKey(const ValueKey('password'));
+        await tester.enterText(passwordTextField, '1234');
+
+        Finder loginButton = find.byType(ElevatedButton);
+        await tester.tap(loginButton);
+        await tester.pumpAndSettle();
+
+        Finder passwordInvalidText = find.text('Please Enter at least 8 character for password');
+
+        // assert
+        expect(passwordInvalidText, findsOneWidget);
+      });
+
+  testWidgets(
+      'should submit form if user email and password is valid',
+          (WidgetTester tester) async {
+        //arrange
+        await tester.pumpWidget(const MaterialApp(
+          home: LoginScreen(),
+        ));
+
+        // act
+        Finder passwordTextField = find.byKey(const ValueKey('password'));
+        await tester.enterText(passwordTextField, 'password');
+        Finder emailTextField = find.byKey(const ValueKey('emailId'));
+        await tester.enterText(emailTextField, 'abc@gmail.com');
+
+        Finder loginButton = find.byType(ElevatedButton);
+        await tester.tap(loginButton);
+        await tester.pumpAndSettle();
+
+        Finder errorTexts = find.text('Required Filed');
+
+        // assert
+        expect(errorTexts, findsNothing);
+      });
+
+}
